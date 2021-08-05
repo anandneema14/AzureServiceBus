61c45a7729568e57f13bb98adbe68861b83800df
Anand Neema <anand.neema14@gmail.com>
2021-08-06 00:42:30 +0530

    Added new projects for storage account demo

---
+++ DeQueueApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Threading.Tasks;
+
+namespace DeQueueApp
+{
+    //Storage Account - Receiving Messages
+    class Program
+    {
+        private const string ConnectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=azstorageanandn;AccountKey=+YGBc+yWLhsBK4svxd+R88Le18FfQSLcn4Y1c7Iy1X/GFTeaS+97PyR3frskXUV+3ys2rzx4EloQ5esxA1etNQ==";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ReceiveArticleAsync();
+            Console.WriteLine($"Received");
+        }
+
+        static async Task<string> ReceiveArticleAsync()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+            CloudQueueClient cloudQueueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue cloudQueue = cloudQueueClient.GetQueueReference("messagequeue");
+
+            bool exists = await cloudQueue.ExistsAsync();
+            if (exists)
+            {
+                CloudQueueMessage retrievedArticle = await cloudQueue.GetMessageAsync();
+                if (retrievedArticle != null)
+                {
+                    string newMessage = retrievedArticle.AsString;
+                    await cloudQueue.DeleteMessageAsync(retrievedArticle);
+                    return newMessage;
+                }
+            }
+            return "Queue Empty or not created";
+        } 
+    }
+}
---
+++ QueueApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Threading.Tasks;
+
+namespace QueueApp
+{
+    //Storage Account - Adding Messages
+    class Program
+    {
+        private const string ConnectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=azstorageanandn;AccountKey=+YGBc+yWLhsBK4svxd+R88Le18FfQSLcn4Y1c7Iy1X/GFTeaS+97PyR3frskXUV+3ys2rzx4EloQ5esxA1etNQ==";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SendArticleAsync("New Message Added 2").Wait();
+            Console.WriteLine($"Sent");
+        }
+
+        static async Task SendArticleAsync(string newsMessage)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+            CloudQueueClient cloudQueueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue cloudQueue = cloudQueueClient.GetQueueReference("messagequeue");
+
+            bool createdQueue = await cloudQueue.CreateIfNotExistsAsync();
+            if (createdQueue)
+            {
+                Console.WriteLine("Queue Created");
+            }
+            CloudQueueMessage queueMessage = new CloudQueueMessage(newsMessage);
+            await cloudQueue.AddMessageAsync(queueMessage);
+            Console.WriteLine("Message sent");
+        }
+    }
+}
