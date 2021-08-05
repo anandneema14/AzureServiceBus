08abd1009405eaa33e0f301789b1c85b18685ec5
Anand Neema <anand.neema14@gmail.com>
2021-08-05 15:51:05 +0530

    Add project files.

---
+++ PrivateMessageSender/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateMessageSender
+{
+    class Program
+    {
+
+        const string ServiceBusConnectionString = "Endpoint=sb://learnservicebusanandn.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=X5rqsFRdelysYRUZpwyu3VOFwk+4oMRAu75vSSt5krU=";
+
+        const string QueueName = "messagesQueue";
+
+        static IQueueClient queueClient;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sending a message to the Sales Messages queue...");
+
+            SendSalesMessageAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Message was sent successfully.");
+        }
+
+        static async Task SendSalesMessageAsync()
+        {
+            //Create Queue Client
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            // Send messages.
+            try
+            {
+                // Send the Message here
+                string messageBody = $"$10,000 order for bicycle parts from retailer Adventure Works.";
+                var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                Console.WriteLine($"Sending message: {messageBody}");
+
+                await queueClient.SendAsync(message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+
+            // Close the connection to the queue here
+            await queueClient.CloseAsync();
+        }
+
+    }
+}
